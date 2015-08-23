c0b2aacb7cce4a6e519908285958cc940209c596
Armin Sander <armin@replicator.org>
2015-08-23 18:02:10 +0200

    initial project setup

---
+++ Fluent.Extensions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fluent.Extensions.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fluent.Extensions.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2015 Armin Sander")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("449c5291-f2b7-4aee-a4b2-d0f060797079")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Fluent.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Fluent.Extensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fluent.Extensions")]
+[assembly: AssemblyCopyright("Copyright © 2015 Armin Sander")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
